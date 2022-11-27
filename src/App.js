04964ab5472286a71e58c4310de73771feced73e
Juan M Hernandez <juanhm93@gmail.com>
2022-11-27 15:25:52 -0500

    finish search and select

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+// import fs from 'fs';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ShoppingCart from './components/ShoppingCart';
 import ToastSoldOut from './components/ToastSoldOut';
 import './App.css';
-import productsJSON from './static/productsJSON.json'
+import productsJSON from './static/productsJSON.json';
+
+
 
 
 function App() {
   const [products, setProducts] = useState(productsJSON.products)
   const [orders, setOrders] = useState([])
   const [show, setShow] = useState(false)
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const getCategories = products.map(product => product.type)
+    const notRepeat = new Set(getCategories)
+    const allCategories = Array.from(notRepeat)
+    setCategories(allCategories)
+  }, [])
 
   const closeToast = () => {
     setShow(false)
@@ -61,17 +74,52 @@ function App() {
     }
   }
 
+  const changeSelected = (e) => {
+    setSelectedCategory(e.target.value)
+  }
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filterProducts = products.filter(product => {
+    if(search !== ''){
+      return product.name.toLowerCase().includes(search.toLowerCase())
+    }else if(selectedCategory !== '' && product.type === selectedCategory){
+      return product
+    }else if(selectedCategory === '' && search === ''){
+      return product
+    }
+  })
+
+  const handleCreateOrder = (totalOrderPrice) => {
+    // const itemsOrder = orders.map(order => {
+    //   return {
+    //     ...order,
+    //     "total_price": order.quantity * order.unit_price
+    //   }
+    // })
+    const createOrder = {
+      "total_order_price": totalOrderPrice,
+      "order": orders.map(order => {
+        return {
+          ...order,
+          "total_price": order.quantity * order.unit_price
+        }
+      })
+    }
+    console.log(createOrder)
+  }
 
   return (
     <div className="App">
-      <Header />
+      <Header categories={categories} changeSelected={changeSelected}  handleSearch={handleSearch} />
       <div className='container-fluid'>
         <div className="row pt-2">
           <div className="ProductList col-sm-6">
-            <ProductList products={products} addToCart={addToCart} />
+            <ProductList products={filterProducts} addToCart={addToCart} />
           </div>
           <div className="col-sm-6">
-            <ShoppingCart orders={orders} />
+            <ShoppingCart orders={orders} createOrder={handleCreateOrder} />
           </div>
         </div>
       </div>
